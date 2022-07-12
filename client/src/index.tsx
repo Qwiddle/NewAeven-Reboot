ec37d9f991373d2677495042c7e7497357f0ba17
Andrew Rubenstein <andjruben@gmail.com>
2022-07-11 21:46:55 -0400

    setup routing for default path "/"

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
