016885377c96be2e6fc52ad907965bd19b8ec09f
Andrew Rubenstein <andjruben@gmail.com>
2022-07-13 05:13:42 -0400

    PreloadScene loading & on load complete callback

--- client/src/scenes/preload.ts
+++ client/src/scenes/preload.ts
@@ -1,16 +1,31 @@
+import WebpackLoader from '../util/webpackLoader';
+import AssetManifest from '../assets/assetManifest';
 import TypeshiftScene from './typeshift';
 import { setStatus } from '../slices/game';
 
 export default class PreloadScene extends TypeshiftScene {
+  loader: any;
+  
   constructor() {
     super('preload');
   }
 
   preload() {
+    this.load.scenePlugin('WebpackLoader', WebpackLoader, 'loader', 'loader');
     this.sceneStore.dispatch(setStatus('load'));
   }
 
   create() {
- 
+    this.loader.start(AssetManifest);
+
+    this.loader.systems.events.on('load', (file: any) => {
+      //handle progressbar
+    });
+    
+    this.loader.load().then(() => {
+      this.sceneStore.dispatch(setStatus('home'));
+    });
+
+    console.log('hi');
   }
 }
