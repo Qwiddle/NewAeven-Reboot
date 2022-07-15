145e54b491111540e6917edf8b136a1cba7470d6
Andrew Rubenstein <andjruben@gmail.com>
2022-07-15 00:55:11 -0400

    complete preloader
    progress bar is drawn & home scene is started when load is complete.

--- client/src/scenes/preload.ts
+++ client/src/scenes/preload.ts
@@ -10,20 +10,59 @@ export default class PreloadScene extends TypeshiftScene {
     super('preload');
   }
 
-  preload() {
+  preload(): void {
     this.load.scenePlugin('WebpackLoader', WebpackLoader, 'loader', 'loader');
     this.sceneStore.dispatch(setStatus('load'));
   }
 
-  create() {
+  create(): void {
+		this.createProgressbar((this.scale.width / 2), (this.scale.height / 2) + 50);
+
     this.loader.start(AssetManifest);
 
-    this.loader.systems.events.on('load', (file: any) => {
-      //handle progressbar
-    });
-    
     this.loader.load().then(() => {
       this.sceneStore.dispatch(setStatus('home'));
+      this.scene.start('home');
     });
   }
+
+  createProgressbar (x: number, y: number): void {
+    let progressLabel: Phaser.GameObjects.Text = this.add.text((this.scale.width / 2), (this.scale.height / 2), 'Loading...', { fontFamily: 'Verdana', align: 'center', fontSize: '12px' });
+		progressLabel.setOrigin(0.5);
+
+		let width: number = 400;
+		let height: number = 20;
+		let xStart: number = x - width / 2;
+		let yStart: number = y - height / 2;
+		let borderOffset: number = 2;
+
+		const borderRect: Phaser.Geom.Rectangle = new Phaser.Geom.Rectangle(
+			xStart - borderOffset,
+			yStart - borderOffset,
+			width + borderOffset * 2,
+			height + borderOffset * 2);
+
+		let border: Phaser.GameObjects.Graphics = this.add.graphics({
+			lineStyle: {
+				width: 5,
+				color: 0xaaaaaa
+			}
+		});
+
+		border.strokeRectShape(borderRect);
+
+		let progressbar: Phaser.GameObjects.Graphics = this.add.graphics();
+
+		const updateProgressbar = (percentage: number) => {
+			progressbar.clear();
+			progressbar.fillStyle(0x007f00, 1);
+			progressbar.fillRect(xStart, yStart, percentage * width, height);
+		};
+
+		this.load.on('progress', updateProgressbar);
+
+    this.loader.systems.events.on('load', (file: any) => {
+      progressLabel.setText("Loading... " + file.url);
+    }); 
+	}
 }
